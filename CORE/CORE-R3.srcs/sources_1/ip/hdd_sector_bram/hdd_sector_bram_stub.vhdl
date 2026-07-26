65cc0f03a186a7b4ddab39d59c9bd930251da374
Samuel <36328867+sho3string@users.noreply.github.com>
2026-07-26 22:43:52 +1000

    hdd_sector_bram

--- CORE/CORE-R3.srcs/sources_1/ip/hdd_sector_bram/hdd_sector_bram_stub.vhdl
+++ CORE/CORE-R3.srcs/sources_1/ip/hdd_sector_bram/hdd_sector_bram_stub.vhdl
@@ -1,7 +1,7 @@
 -- Copyright 1986-2022 Xilinx, Inc. All Rights Reserved.
 -- --------------------------------------------------------------------------------
 -- Tool Version: Vivado v.2022.2 (lin64) Build 3671981 Fri Oct 14 04:59:54 MDT 2022
--- Date        : Tue Jul  7 15:50:44 2026
+-- Date        : Sun Jul 26 22:25:28 2026
 -- Host        : shoestring-virtual-machine running 64-bit Ubuntu 22.04.4 LTS
 -- Command     : write_vhdl -force -mode synth_stub
 --               /mnt/hgfs/GitHub/Apple-II-MEGA65/CORE/CORE-R3.srcs/sources_1/ip/hdd_sector_bram/hdd_sector_bram_stub.vhdl
